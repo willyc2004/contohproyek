a3a673f07994495e7e6149b1fc9ac655f332f056
willyc2004 <willy.cahyadi25@gmail.com>
2023-10-02 09:47:33 +0700

    Update Laravel pake Blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,26 @@
-@extends('layouts.template')
+<x-frame>
 
-@section('layout_tagline' , 'Welcome to SomeEggs Library')
-@section('layout_content')
+    <x-slot:layout_pagetitle>
+        {{ $pagetitle }} 
+    </x-slot:layout_pagetitle>
 
+    <x-slot:layout_maintitle>
+        {{ $maintitle }} 
+    </x-slot:layout_maintitle>
+
+    <x-slot:layout_tagline>
+        Welcome to SomeEggs Library
+    </x-slot:layout_tagline>
     <h3>Welcome</h3>
     <p>This application is to manage my mini library</p>
     <p>This application is to manage my mini library</p>
     <p>This application is to manage my mini library</p>
 
-    <h3>Welcome</h3>
+    <h3>Hello</h3>
     <p>This application is to manage my mini library</p>
     <p>This application is to manage my mini library</p>
     <p>This application is to manage my mini library</p>
-@endsection
+
+    <x-mybutton>Contact Us</x-mybutton>
+
+</x-frame>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index', [
         "pagetitle" => "Home", 
-        "maintitle" => "Welcome to SomeEggs Library"
+        "maintitle" => "My Library"
     ]);
 });
 
